8b051813953fb3db901bc02b04afc2f3bd9c8bcc
Алексей <nox-1987@mail.ru>
2022-10-20 13:13:25 +0300

    добавил comment и follow в админку

--- yatube/posts/admin.py
+++ yatube/posts/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 
-from .models import Post
-from .models import Group
+from .models import Post, Group, Comment, Follow
 
 
 class PostAdmin(admin.ModelAdmin):
@@ -34,5 +33,30 @@ class GroupAdmin(admin.ModelAdmin):
     empty_value_display = "-пусто-"
 
 
+class CommentAdmin(admin.ModelAdmin):
+    list_display = (
+        "pk",
+        "text",
+        "author",
+        "pub_date",
+    )
+    search_fields = ("text",)
+    list_filter = ("author",)
+
+
+class FollowAdmin(admin.ModelAdmin):
+    list_display = (
+        "pk",
+        "user",
+        "author",
+    )
+    list_filter = (
+        "user",
+        "author",
+    )
+
+
 admin.site.register(Post, PostAdmin)
 admin.site.register(Group, GroupAdmin)
+admin.site.register(Comment, CommentAdmin)
+admin.site.register(Follow, FollowAdmin)
