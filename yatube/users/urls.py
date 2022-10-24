ea10c16ecf7ddfdf3cb0310e385b40c2ff5f65f7
Трофимов Алексей <9939713@mail.ru>
2022-10-24 22:32:53 +0300

    Добавил PasswordChangeView

--- yatube/users/urls.py
+++ yatube/users/urls.py
@@ -1,5 +1,10 @@
 # Импортируем из приложения django.contrib.auth нужный view-класс
-from django.contrib.auth.views import LoginView, LogoutView, PasswordResetView
+from django.contrib.auth.views import (
+    LoginView,
+    LogoutView,
+    PasswordResetView,
+    PasswordChangeView,
+)
 from django.urls import path
 
 from . import views
@@ -7,17 +12,12 @@ from . import views
 app_name = "users"
 
 urlpatterns = [
+    path("signup/", views.SignUp.as_view(), name="signup"),
     path(
         "logout/",
-        # Прямо в описании обработчика укажем шаблон,
-        # который должен применяться для отображения возвращаемой страницы.
-        # Да, во view-классах так можно! Как их не полюбить.
         LogoutView.as_view(template_name="users/logged_out.html"),
         name="logout",
     ),
-    # Полный адрес страницы регистрации - auth/signup/,
-    # но префикс auth/ обрабатывется в головном urls.py
-    path("signup/", views.SignUp.as_view(), name="signup"),
     path(
         "login/",
         LoginView.as_view(template_name="users/login.html"),
@@ -28,6 +28,13 @@ urlpatterns = [
         PasswordResetView.as_view(
             template_name="users/password_reset_form.html"
         ),
-        name="password_reset_form",
+        name="password_reset",
+    ),
+    path(
+        "password_change/",
+        PasswordChangeView.as_view(
+            template_name="users/password_change_form.html"
+        ),
+        name="password_change",
     ),
 ]
