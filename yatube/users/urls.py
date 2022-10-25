a2e419105042d213f06c4d6da6c7bdeec562e902
Трофимов Алексей <9939713@mail.ru>
2022-10-25 21:30:30 +0300

    add password_reset_confirm и password_reset_comple

--- yatube/users/urls.py
+++ yatube/users/urls.py
@@ -6,6 +6,8 @@ from django.contrib.auth.views import (
     PasswordChangeView,
     PasswordChangeDoneView,
     PasswordResetDoneView,
+    PasswordResetConfirmView,
+    PasswordResetCompleteView,
 )
 from django.urls import path
 
@@ -37,7 +39,21 @@ urlpatterns = [
         PasswordResetDoneView.as_view(
             template_name="users/password_reset_done.html"
         ),
-        name="password_reset",
+        name="password_reset_done",
+    ),
+    path(
+        "reset/<uidb64>/<token>/",
+        PasswordResetConfirmView.as_view(
+            template_name="users/password_reset_confirm.html"
+        ),
+        name="password_reset_confirm",
+    ),
+    path(
+        "reset/done/",
+        PasswordResetCompleteView.as_view(
+            template_name="users/password_reset_complete.html"
+        ),
+        name="password_reset_complete",
     ),
     path(
         "password_change/",
